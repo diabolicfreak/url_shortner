eb9d92cd2b64ae07e7ab533b8abfaf23bda0c094
diabolicfreak <diabolicfreak@gmail.com>
2017-07-15 23:00:02 +0530

    Added url redirect to template

--- src/shortner/views.py
+++ src/shortner/views.py
@@ -1,13 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from django.views import View
+from .models import ShortnerURL
 
 # Create your views here.
 def shortner_url_view(response, shortcode=None, *args, **kwargs):
-    print(shortcode)
-    return HttpResponse('Hello method')
+    obj = get_object_or_404(ShortnerURL, shortcode=shortcode)
+    return HttpResponse('Redirect url is {}'.format(obj.url))
 
 class ShortnerCBView(View):
     def get(self, request, shortcode=None, *args, **kwargs):
-        print(shortcode)
-        return HttpResponse('Hello cbv')
+        obj = get_object_or_404(ShortnerURL, shortcode=shortcode)
+        return HttpResponse('Redirect url is {}'.format(obj.url))
